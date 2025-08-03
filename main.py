6d184e92467095b93df3710ae0e6e1c275831b7e
Михайло <miva42526gg@gmail.com>
2025-08-03 20:32:09 +0200

    1.1

--- main.py
+++ main.py
@@ -14,8 +14,33 @@
 #print(age + 1)
 
 
-my_lucky_number = 7
-guess = int(input("Guess my lucky number! I think it is: "))
-while my_lucky_number != guess:
-     guess = int(input("Oops! Not it. Try again: "))
-print("Congrats! You guessed it.")
+#my_lucky_number = 7
+#guess = int(input("Guess my lucky number! I think it is: "))
+#while my_lucky_number != guess:
+#     guess = int(input("Oops! Not it. Try again: "))
+#print("Congrats! You guessed it.")
+
+
+#name = input("What's your name? ")
+
+#hi = f"Hi, {name}!"  f это метод для форматирование строк так тупо быстрее 
+
+#print(hi)
+
+# Встановлюємо ціни на продукти
+
+#name = input("Your name? ")
+#email = input("Your email? ")
+#age = int(input("Your age? "))
+#height = float(input("Your height? "))
+#is_active = True
+
+name = "Taras"
+age = 22
+has_driver_licence = True
+
+if name and age >= 18 and has_driver_licence:
+    print(f"User {name} can rent a car")
+else:
+    print(f"User {name} cannot rent a car")
+
